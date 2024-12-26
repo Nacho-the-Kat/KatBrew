23ebb396c1b331c2ed6427e73009ecfd034f411b
CrixusHD <nick.engl@gmx.de>
2024-12-26 20:32:48 +0100

    wfs

---
+++ workflows/src/main/java/org/katbrew/tasks/FetchTokens.java
@@ -0,0 +1,13 @@
+package org.katbrew.tasks;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Component("fetchTokens")
+public class FetchTokens implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        System.out.println("Fetching token starts");
+    }
+}
