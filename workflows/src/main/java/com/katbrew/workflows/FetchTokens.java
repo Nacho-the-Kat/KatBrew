b6f074e1d6935ba46af7cbd2cee395fff7c84c95
“CrixusHD“ <nick.engl@gmx.de>
2024-12-27 12:30:40 +0100

    task

--- workflows/src/main/java/com/katbrew/workflows/FetchTokens.java
+++ workflows/src/main/java/com/katbrew/workflows/FetchTokens.java
@@ -3,14 +3,12 @@ package com.katbrew.workflows;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.katbrew.entities.jooq.db.tables.pojos.Token;
 import com.katbrew.helper.KatBrewObjectMapper;
-import com.katbrew.services.base.ApiResponse;
+import lombok.Data;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.json.JSONObject;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.util.List;
 
@@ -21,12 +19,21 @@ public class FetchTokens implements JavaDelegate {
     @Override
     public void execute(DelegateExecution execution) throws Exception {
         WebClient client = WebClient.builder().baseUrl("https://devkatapi.nachowyborski.xyz").build();
-        String te = client.get().uri("/api/token/tokenlist?limit=10").retrieve().bodyToMono(String.class).block();
-//        System.out.println(te.getResult().size());        new Parameterized
-//        te.getResult().forEach(Token::getTick);
-        JSONObject obj = mapper.convertValue(te, JSONObject.class);
-
-        List<Token> e = mapper.convertValue(obj.get("result") , new TypeReference<List<Token>>(){});
+        //todo edit uri
+        Response<List<Token>> tokenList = client
+                .get()
+                .uri("/api/token/tokenlist?limit=10")
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Response<List<Token>>>() {})
+                .block();
+        //todo
         System.out.println("Fetching token starts");
+    }
+
+    @Data
+    private static class Response<T> {
+        T result;
+        Integer cursor;
+
     }
 }
